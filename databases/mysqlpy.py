5c75d7d061ff5f6a06ab612464b06a6724f104f5
faustinok <faustinoknada@gmail.com>
2024-10-01 15:04:38 -0300

    sessao de banco de dados 01/10

--- databases/mysqlpy.py
+++ databases/mysqlpy.py
@@ -2,6 +2,14 @@ import pymysql
 import dotenv
 import os
 dotenv.load_dotenv()
+json_ = {"nome" : "gabriel", "idade" : 28}
+sql = (
+                f' insert into clientes'
+                '('
+                'nome,idade,'
+                ')'
+                'values(%(nome)s,%(idade)s)'
+            )
 connection = pymysql.connect(
     host= os.environ['BD_HOST'],
     user=os.environ['BD_USER'],
@@ -28,3 +36,10 @@ with connection:
                 'values("gabriel",12)'
             )
             connection.commit()        
+
+# inserindo como json
+    with connection.cursor() as cursor:
+            cursor.execute(sql,json_)
+            connection.commit()  
+# ler e igual ao sql_lote.py
+
